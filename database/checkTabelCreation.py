692939b76be02c841b9c12383b17872506ce907d
Ubuntu <ubuntu@ip-192-168-55-188.ap-northeast-1.compute.internal>
2024-11-28 06:38:05 +0000

    add workflow

---
+++ database/checkTabelCreation.py
@@ -0,0 +1,60 @@
+import pyodbc
+
+# Connection setup
+conn = pyodbc.connect(
+    "Driver={ODBC Driver 18 for SQL Server};"
+    "Server=cmswang.database.windows.net;"
+    "Database=cms;"
+    "Uid=cmsadmin;"
+    "Pwd=CMS4dmin;"
+    "Encrypt=yes;"
+    "TrustServerCertificate=no;"
+)
+
+cursor = conn.cursor()
+
+print("-----------------------------------")
+cursor.execute("SELECT DB_NAME();")
+print("Current Database:", cursor.fetchone()[0])
+print("-----------------------------------")
+print("")
+
+cursor.execute("""
+    SELECT TABLE_SCHEMA, TABLE_NAME
+    FROM INFORMATION_SCHEMA.TABLES
+    WHERE TABLE_TYPE = 'BASE TABLE';
+""")
+
+print("-----------------------------------")
+print("Tables in the Database:")
+for row in cursor.fetchall():
+    print(row)
+print("-----------------------------------")
+print("")
+
+# List tables
+cursor.execute("""
+    SELECT * FROM dbo.users;
+""")
+
+print("-----------------------------------")
+print("Records in the users table:")
+for row in cursor.fetchall():
+    print(row)
+print("-----------------------------------")
+print("")
+
+cursor.execute("""
+    SELECT * FROM dbo.posts;
+""")
+
+print("-----------------------------------")
+print("Records in the posts table:")
+for row in cursor.fetchall():
+    print(row)
+print("-----------------------------------")
+print("")
+
+# Close the connection
+conn.close()
+
