b8d123a2eb506b945aa612a5ff7bda5a2eb8b782
Ubuntu <ubuntu@ip-192-168-55-188.ap-northeast-1.compute.internal>
2024-11-29 03:10:35 +0000

    fix app name

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -95,7 +95,7 @@ resource "azurerm_app_service_plan" "app_service_plan" {
 
 # App Service
 resource "azurerm_linux_web_app" "app_service" {
-  name                = "python-web-app"
+  name                = "pytestwang-app"
   location            = azurerm_resource_group.cms.location
   resource_group_name = azurerm_resource_group.cms.name
   service_plan_id     = azurerm_app_service_plan.app_service_plan.id
