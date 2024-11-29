f1d57bba801f4a9b83fd976c9881fcc1f02f14e7
Ubuntu <ubuntu@ip-192-168-55-188.ap-northeast-1.compute.internal>
2024-11-29 01:51:32 +0000

    change app service

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -88,8 +88,8 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   location            = azurerm_resource_group.cms.location
   resource_group_name = azurerm_resource_group.cms.name
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Basic"
+    size = "B1"
   }
 }
 
