c5308afd8ebc230a6ba160c95bbb45bdbdaeee28
Ubuntu <ubuntu@ip-192-168-55-188.ap-northeast-1.compute.internal>
2024-11-29 09:46:05 +0000

    fix repo

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -120,8 +120,9 @@ def _save_cache(cache):
     pass
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
-    return None
+    app=msal.ConfidentialClientApplication(Config.CLIENT_ID,authority=authority or Config.AUTHORITY,
+    client_credential=Config.CLIENT_SECRET,token_cache=cache)
+    return app
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -112,7 +112,7 @@ resource "azurerm_linux_web_app" "webapp" {
 
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com/wanghongran2023/Azure-CMS-for-articles"
+  repo_url           = "https://github.com/wanghongran2023/Azure-CMS-for-articles.git"
   branch             = "main"
   use_manual_integration = false
   use_mercurial      = false
