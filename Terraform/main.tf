57e3413f020297f4091548969c8bb9a9c4d9adeb
Ubuntu <ubuntu@ip-192-168-55-188.ap-northeast-1.compute.internal>
2024-11-29 03:42:15 +0000

    add parameter for app name

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -95,7 +95,7 @@ resource "azurerm_app_service_plan" "app_service_plan" {
 
 # App Service
 resource "azurerm_linux_web_app" "app_service" {
-  name                = "pytestwang-app"
+  name                = var.app_config.name
   location            = azurerm_resource_group.cms.location
   resource_group_name = azurerm_resource_group.cms.name
   service_plan_id     = azurerm_app_service_plan.app_service_plan.id
