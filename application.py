9b5a40cab70547fe1f9bb5d66033b210c628e113
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-11-30 12:36:28 +0900

    Update application.py

--- application.py
+++ application.py
@@ -2,9 +2,12 @@
 This script runs the FlaskWebProject application using a development server.
 """
 
+"""
 from os import environ
+"""
 from FlaskWebProject import app
 
+"""
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
@@ -12,3 +15,4 @@ if __name__ == '__main__':
     except ValueError:
         PORT = 8000
     app.run(HOST, PORT, ssl_context='adhoc')
+"""
