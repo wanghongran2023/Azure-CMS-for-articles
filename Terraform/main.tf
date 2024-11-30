4c85083101338e1303279887c4b7c42e9e87c7d6
Ubuntu <ubuntu@ip-192-168-55-188.ap-northeast-1.compute.internal>
2024-11-30 00:22:46 +0000

    fix

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -88,7 +88,6 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   name                = "python-app-service-plan"
   location            = azurerm_resource_group.cms.location
   resource_group_name = azurerm_resource_group.cms.name
-  os_type             = "Linux"
   reserved            = true
   sku {
     tier = "PremiumV3"
